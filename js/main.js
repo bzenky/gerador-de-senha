062d80d96c94c129776dc1dc3bc81fcb93644d0a
Zenky <bzenky@gmail.com>
2021-08-08 14:30:10 -0300

    copy to clipboard update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
 const senha = document.querySelector('.senha')
 const gerar = document.querySelector('.gerar')
+const copiar = document.querySelector('.copiar')
+const toasty = document.querySelector('.toasty')
 
 function criarSenha() {
-  senha.innerHTML = ''
+  senha.value = ''
   const alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
   const numerais = "0123456789"
   const especiais = "!@#$%&()*+,-./"
   const variaveis = alfabeto + numerais + especiais
 
+  copiar.classList.add('visible')
 
   for (let cont = 0; cont <= 7; cont++) {
-    senha.innerHTML += variaveis[Math.floor(Math.random() * variaveis.length)] 
+    senha.value += variaveis[Math.floor(Math.random() * variaveis.length)] 
   }
 }
 
 gerar.addEventListener('click', criarSenha)
+
+
+function copiarSenha() {
+  const senha = document.querySelector('.senha');
+
+  
+  senha.select()
+  senha.setSelectionRange(0, 99999)
+  
+  document.execCommand("copy")
+  
+  toasty.classList.add('active')
+
+  setTimeout(() => {
+    toasty.classList.remove('active')
+  }, 1200)
+
+}
+
+copiar.addEventListener('click', copiarSenha)
+
+
